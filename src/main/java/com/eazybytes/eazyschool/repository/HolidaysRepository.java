ae37df88cb2594aac46131192041ff38c24917c0
Sanket Rathod <sanketra53@gmail.com>
2025-12-22 00:02:31 +0530

    Fifth Commit

---
+++ src/main/java/com/eazybytes/eazyschool/repository/HolidaysRepository.java
@@ -0,0 +1,24 @@
+package com.eazybytes.eazyschool.repository;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.model.Holiday;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Repository
+public interface HolidaysRepository extends CrudRepository<Holiday, String> {
+
+
+
+}
