e5dc428a6d47ee2548f40708f7698c6955d817b9
Sanket Rathod <sanketra53@gmail.com>
2025-12-25 00:17:45 +0530

    Added the Class section with the students.

--- src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
@@ -1,23 +1,102 @@
 package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.EazyClass;
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.repository.EduSphereRepository;
+import com.eazybytes.eazyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
 @RequestMapping("admin")
 public class AdminController {
 
-        @RequestMapping("/displayClasses")
-        public ModelAndView displayClasses(Model model){
-            ModelAndView modelAndView = new ModelAndView("classes.html");
-            modelAndView.addObject("eazyClass", new EazyClass());
-            return modelAndView;
-        }
+    @Autowired
+    EduSphereRepository eduSphereRepository;
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @RequestMapping("/displayClasses")
+    public ModelAndView displayClasses(Model model) {
+        List<EazyClass> eazyClasses = eduSphereRepository.findAll();
+        ModelAndView modelAndView = new ModelAndView("classes.html");
+        modelAndView.addObject("eazyClasses",eazyClasses);
+        modelAndView.addObject("eazyClass", new EazyClass());
+        return modelAndView;
     }
 
+    @PostMapping("/addNewClass")
+    public ModelAndView addNewClass(Model model, @ModelAttribute("eazyClass") EazyClass eazyClass) {
+        eduSphereRepository.save(eazyClass);
+        ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayClasses");
+        return modelAndView;
+    }
+
+    @RequestMapping("/deleteClass")
+    public ModelAndView deleteClass(Model model, @RequestParam int id) {
+        Optional<EazyClass> eazyClass = eduSphereRepository.findById(id);
+        for(Person person : eazyClass.get().getPersons()){
+            person.setEazyClass(null);
+            personRepository.save(person);
+        }
+        eduSphereRepository.deleteById(id);
+        ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayClasses");
+        return modelAndView;
+    }
+
+    @GetMapping("/displayStudents")
+    public ModelAndView displayStudents(Model model, @RequestParam int classId, HttpSession session,
+                                        @RequestParam(value = "error", required = false) String error) {
+        String errorMessage = null;
+        ModelAndView modelAndView = new ModelAndView("students.html");
+        Optional<EazyClass> eazyClass = eduSphereRepository.findById(classId);
+        modelAndView.addObject("eazyClass",eazyClass.get());
+        modelAndView.addObject("person",new Person());
+        session.setAttribute("eazyClass",eazyClass.get());
+        if(error != null) {
+            errorMessage = "Invalid Email entered!!";
+            modelAndView.addObject("errorMessage", errorMessage);
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/addStudent")
+    public ModelAndView addStudent(Model model, @ModelAttribute("person") Person person, HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView();
+        EazyClass eazyClass = (EazyClass) session.getAttribute("eazyClass");
+        Person personEntity = personRepository.readByEmail(person.getEmail());
+        if(personEntity==null || !(personEntity.getPersonId()>0)){
+            modelAndView.setViewName("redirect:/admin/displayStudents?classId="+eazyClass.getClassId()
+                    +"&error=true");
+            return modelAndView;
+        }
+        personEntity.setEazyClass(eazyClass);
+        personRepository.save(personEntity);
+        eazyClass.getPersons().add(personEntity);
+        eduSphereRepository.save(eazyClass);
+        modelAndView.setViewName("redirect:/admin/displayStudents?classId="+eazyClass.getClassId());
+        return modelAndView;
+    }
+
+    @GetMapping("/deleteStudent")
+    public ModelAndView deleteStudent(Model model, @RequestParam int personId, HttpSession session) {
+        EazyClass eazyClass = (EazyClass) session.getAttribute("eazyClass");
+        Optional<Person> person = personRepository.findById(personId);
+        person.get().setEazyClass(null);
+        eazyClass.getPersons().remove(person.get());
+        EazyClass eazyClassSaved = eduSphereRepository.save(eazyClass);
+        session.setAttribute("eazyClass",eazyClassSaved);
+        ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayStudents?classId="+eazyClass.getClassId());
+        return modelAndView;
+    }
+}
--- src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
+++ src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
@@ -12,7 +12,7 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
-@Table(name = "class")
+@Table(name = "classes")
 public class EazyClass extends BaseEntity {
 
     @Id
